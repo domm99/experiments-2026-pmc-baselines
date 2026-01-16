7dd841e577e1677d55fc5af1262154fa48ca6bbe
Davide Domini <davide.domini@unibo.it>
2026-01-16 15:41:10 +0100

    chore: modified hyperparams

--- src/main.py
+++ src/main.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
 
     total_experiments = 0
 
-    datasets        = ['EMNIST'] # TODO - add all datasets
-    algorithms      = ['fedavg','fedprox', 'scaffold', 'ifca']
+    datasets        = ['EMNIST']
+    algorithms      = ['ifca'] #['fedavg','fedprox', 'scaffold', 'ifca']
     areas           = [3, 5, 9]
     partitionings   = ['Hard', 'Dirichlet']
     sparsifications = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.99]
